01e22bf5936b3f3615d07a9316ccfb12bad8f018
Renaat De Vos <renaat@drvn.be>
2023-05-23 13:26:40 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket::Request;
+
 #[cfg(test)] mod tests;
 
 #[get("/")]
@@ -9,6 +11,29 @@ fn hello() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/api/v1/answer")]
+fn answer() -> &'static str {
+    return get_answer();
+}
+
+fn get_answer() -> &'static str {
+    return "42";
+}
+
+#[catch(500)]
+fn internal_error() -> &'static str {
+    "Whoops! Looks like we messed up."
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("I couldn't find '{}'. Try something else?", req.uri())
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/", routes![hello])
+        .mount("/", routes![answer])
+        .register(catchers![internal_error, not_found])
+        .launch();
 }
--- src/tests.rs
+++ src/tests.rs
@@ -8,3 +8,27 @@ fn hello_world() {
     let mut response = client.get("/").dispatch();
     assert_eq!(response.body_string(), Some("Hello, world!".into()));
 }
+
+#[test]
+fn answer() {
+    let rocket = rocket::ignite().mount("/", routes![super::answer]);
+    let client = Client::new(rocket).unwrap();
+    let mut response = client.get("/api/v1/answer").dispatch();
+    assert_eq!(response.body_string(), Some("42".into()));
+}
+
+#[test]
+fn not_found() {
+    let rocket = rocket::ignite().mount("/", routes![super::hello]);
+    let client = Client::new(rocket).unwrap();
+    let mut response = client.get("/i_do_not_exist").dispatch();
+    assert_eq!(response.body_string(), Some("I couldn't find '/i_do_not_exist'. Try something else?".into()));
+}
+
+// #[test]
+// fn internal_error() {
+//     let rocket = rocket::ignite().mount("/", routes![super::answer]);
+//     let client = Client::new(rocket).unwrap();
+//     let mut response = client.get("/api/v1/answer").dispatch();
+//     assert_eq!(response.body_string(), Some("Whoops! Looks like we messed up.".into()));
+// }
